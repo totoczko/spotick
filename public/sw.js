97bf0abaf29ad8d4580b29ebdcb1607e6bd4dc6d
totoczko <m.toloczko1@gmail.com>
2019-03-02 23:11:45 +0100

    initial caching

--- public/sw.js
+++ public/sw.js
@@ -3,6 +3,28 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
 
+    workbox.precaching.precacheAndRoute([]);
+
+    importScripts(
+        "/precache-manifest.5683773f26cb2d8ce534cde80cf446ce.js"
+    );
+
+    workbox.clientsClaim();
+
+    /**
+     * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+     * requests for URLs in the manifest.
+     * See https://goo.gl/S9QRab
+     */
+    self.__precacheManifest = [].concat(self.__precacheManifest || []);
+    workbox.precaching.suppressWarnings();
+    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+    workbox.routing.registerNavigationRoute("/index.html", {
+        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+    });
+
+
     // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
     workbox.routing.registerRoute(
         /^https:\/\/fonts\.googleapis\.com/,
@@ -42,7 +64,14 @@ if (workbox) {
     );
 
     workbox.routing.registerRoute(
-        /\.(?:js|css)$/,
+        /index.html/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'html',
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /\.(?:js|css|html)$/,
         new workbox.strategies.StaleWhileRevalidate({
             cacheName: 'static-resources',
         })
@@ -54,6 +83,14 @@ if (workbox) {
             cacheName: 'post-images',
         })
     );
+
+    workbox.routing.registerRoute(
+        /.*.firebaseio\.com.*/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'posts',
+        })
+    );
+
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
