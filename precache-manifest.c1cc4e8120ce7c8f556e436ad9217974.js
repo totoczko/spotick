39608cb67db3bbe2e938b5db2e3458567400dd45
totoczko <m.toloczko1@gmail.com>
2019-03-10 16:16:02 +0100

    Updates

---
+++ precache-manifest.c1cc4e8120ce7c8f556e436ad9217974.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f559a569b1824ddb636",
+    "url": "/spotick/static/js/runtime~main.0f559a56.js"
+  },
+  {
+    "revision": "f92cd41ea5620f1c5362",
+    "url": "/spotick/static/js/main.f92cd41e.chunk.js"
+  },
+  {
+    "revision": "d1a0e72392accb1dc9af",
+    "url": "/spotick/static/js/1.d1a0e723.chunk.js"
+  },
+  {
+    "revision": "f92cd41ea5620f1c5362",
+    "url": "/spotick/static/css/main.fcc796a4.chunk.css"
+  },
+  {
+    "revision": "ffa8ac8293a371c768cee595de71987a",
+    "url": "/spotick/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spotick/precache-manifest.a1ec7d3a3a02be6128a82b40e38802a8.js"
+  "/spotick/precache-manifest.c1cc4e8120ce7c8f556e436ad9217974.js"
 );
 
 workbox.clientsClaim();
