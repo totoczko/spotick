7c7ec97ae6574db18383f136682337b3ef572007
totoczko <m.toloczko1@gmail.com>
2019-03-10 14:34:12 +0100

    Updates

---
+++ precache-manifest.eb6835823c887c2e72e89c58a34bb88e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f559a569b1824ddb636",
+    "url": "/spotick/static/js/runtime~main.0f559a56.js"
+  },
+  {
+    "revision": "48355c6049db228d1b5a",
+    "url": "/spotick/static/js/main.48355c60.chunk.js"
+  },
+  {
+    "revision": "81aba09dadbfae22dbe3",
+    "url": "/spotick/static/js/1.81aba09d.chunk.js"
+  },
+  {
+    "revision": "48355c6049db228d1b5a",
+    "url": "/spotick/static/css/main.c06cf3ef.chunk.css"
+  },
+  {
+    "revision": "50e8dc735a93f823ea4728db7c44d19d",
+    "url": "/spotick/index.html"
+  }
+];
