e1d17ecb0a30b22bdbfd6c4f70e1c7df699a932a
totoczko <m.toloczko1@gmail.com>
2019-03-29 19:26:58 +0100

    build

---
+++ build/precache-manifest.0b3d5ffbf81b83ae06eef64a1ac0bd51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f559a569b1824ddb636",
+    "url": "/spotick/static/js/runtime~main.0f559a56.js"
+  },
+  {
+    "revision": "ee8e08fe7a287887a1be",
+    "url": "/spotick/static/js/main.ee8e08fe.chunk.js"
+  },
+  {
+    "revision": "6d7959a36041e78e17e1",
+    "url": "/spotick/static/js/1.6d7959a3.chunk.js"
+  },
+  {
+    "revision": "ee8e08fe7a287887a1be",
+    "url": "/spotick/static/css/main.b126b3f1.chunk.css"
+  },
+  {
+    "revision": "37a095be743e7cecb7b09c02e0af32de",
+    "url": "/spotick/index.html"
+  }
+];
---
+++ build/sw.js
@@ -0,0 +1,149 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.precaching.precacheAndRoute([]);
+
+    // importScripts(
+    //     "/precache-manifest.5683773f26cb2d8ce534cde80cf446ce.js"
+    // );
+
+    workbox.clientsClaim();
+
+    /**
+     * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+     * requests for URLs in the manifest.
+     * See https://goo.gl/S9QRab
+     */
+    self.__precacheManifest = [].concat(self.__precacheManifest || []);
+    workbox.precaching.suppressWarnings();
+    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+    workbox.routing.registerNavigationRoute("./index.html", {
+        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+    });
+
+
+    // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets',
+        })
+    );
+
+    // Cache the underlying font files with a cache-first strategy for 1 year.
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'google-fonts-webfonts',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /\.(?:png|gif|jpg|jpeg|svg)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'images',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                }),
+            ],
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /index.html/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'html',
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /\.(?:js|css|html)$/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'static-resources',
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /^https:\/\/firebasestorage\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'post-images',
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /.*.firebaseio\.com.*/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'posts',
+        })
+    );
+
+    self.addEventListener('notificationclick', (e) => {
+        const notification = e.notification;
+        const action = e.action;
+        console.log(notification);
+        if (action === 'confirm') {
+            console.log('confirm was chosen')
+            notification.close();
+        } else {
+            console.log(action)
+            e.waitUntil(clients.matchAll().then((clis) => {
+                let client = clis.find((c) => {
+                    return c.visibilityState === 'visible'
+                })
+
+                if (client !== undefined) {
+                    client.navigate('http://localhost:3000')
+                    client.focus()
+                } else {
+                    clients.openWindow('http://localhost:3000')
+                }
+                notification.close()
+            }))
+        }
+    })
+
+    self.addEventListener('push', (event) => {
+        console.log('push notification received', event);
+
+        let data = {
+            title: 'New!',
+            constent: 'New post on Spotick!'
+        };
+        if (event.data) {
+            data = JSON.parse(event.data.text());
+        }
+
+        const options = {
+            body: data.content,
+            icon: 'img/icons/icon-96x96.png',
+            vibrate: [100, 50, 200],
+            badge: 'img/icons/icon-96x96.png',
+            tag: 'new-post',
+            renotify: true
+        }
+        event.waitUntil(
+            self.registration.showNotification(data.title, options)
+        )
+
+    })
+
+    console.log('1')
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
