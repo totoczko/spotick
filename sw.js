88a5957f322e908aa133ac600a3c82c722510659
totoczko <m.toloczko1@gmail.com>
2019-04-01 09:55:33 +0200

    Updates

--- sw.js
+++ sw.js
@@ -85,18 +85,18 @@ if ('function' === typeof importScripts) {
       blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
     });
 
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg)$/,
-      workbox.strategies.cacheFirst({
-        cacheName: 'images',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 60,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          }),
-        ],
-      })
-    );
+    // workbox.routing.registerRoute(
+    //   /\.(?:png|gif|jpg|jpeg)$/,
+    //   workbox.strategies.cacheFirst({
+    //     cacheName: 'images',
+    //     plugins: [
+    //       new workbox.expiration.Plugin({
+    //         maxEntries: 60,
+    //         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    //       }),
+    //     ],
+    //   })
+    // );
 
     //start
 
